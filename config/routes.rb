d17577cf8bc7c211c474d2680f0de653be0270fc
nakaearth <naka5313@gmail.com>
2017-10-27 18:25:04 +0900

    追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'top#index'
+
+  # Facebook login
+  get "/:provider/login"  => "sessions#new"
+  get "/logout" => "sessions#destroy"
+  get "/auth/:provider/callback" => "sessions#create" unless Rails.env.development?
+  post "/auth/:provider/callback" => "sessions#create" if Rails.env.development?
+  get "/auth/failure" => "sessions#failuer"
+
 end
