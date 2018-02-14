c6230f4417a949dcde49580e0b470d9ca0e63c98
nakaearth <naka5313@gmail.com>
2018-02-14 18:53:00 +0900

    検索接続周りのロジックを変更

--- app/services/search/base_service.rb
+++ app/services/search/base_service.rb
@@ -3,12 +3,7 @@
 module Search
   class BaseService
     def initialize(search_model_class, params)
-      if search_model_class.columns { |column| column.type == 'text' }.any?
-        @client = search_model_class.__elasticsearch__
-      else
-        raise "Format error: #{search_model_class} do not have text type column"
-      end
-
+      @client = search_model_class.__elasticsearch__
       @conditions = params
     end
   end
