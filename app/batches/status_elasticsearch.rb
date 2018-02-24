8b0da1802b742849de7dc4a3dcce0a83d1b05db0
nakaearth <naka5313@gmail.com>
2018-02-24 18:31:48 +0900

    rubocop対応

--- app/batches/status_elasticsearch.rb
+++ app/batches/status_elasticsearch.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
+
 # bundle exec rails runner StatusElasticsearch.execute
 #
 class StatusElasticsearch
   class << self
     def execute
       logger = ActiveSupport::Logger.new("log/setting_batch.log", 'daily')
-      force = args[:force] || false
-
       logger.info('index作成')
       logger.info('========= indexのstatusを表示 =========')
       status = ElasticsearchClient.client.indices.status
