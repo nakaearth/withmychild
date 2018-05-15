63a037da765e12fac9bf4590b2e49ea5886fc6ea
nakaearth <naka5313@gmail.com>
2018-05-16 06:58:15 +0900

    チューニング

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { PlaceList } from './placeList'
 
-console.log(document.getElementById('paceList'))
-ReactDOM.render('<PlaceList />', document.getElementById('placeList'))
+console.log(document.getElementById('placeList'));
+ReactDOM.render('<PlaceList />', document.getElementById('placeList'));
