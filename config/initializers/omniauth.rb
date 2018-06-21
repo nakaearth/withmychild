68931f3b88511b74458395ab229922c5be31a3c2
nakaearth <naka5313@gmail.com>
2018-06-21 19:14:17 +0900

    google loginを追加

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,5 +2,11 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
   provider :twitter, ENV["TWITTER_FAMIPHOTOS_API_TOKEN"], ENV["TWITTER_FAMIPHOTOS_SECRET_TOKEN"]
+  provider :google_oauth2, ENV["GOOGLE_APP_ID"], ENV["GOOGLE_APP_SECRET"],
+    {
+      scope: 'email',
+      prompt: 'select_account',
+      image_aspect_ratio: 'square',
+    }
 end
 # OmniAuth.config.on_failure = SessionsController.action(:oauth_failure)
